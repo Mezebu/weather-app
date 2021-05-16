8789ec66d1688c70b1667d55cf949bf39d02cccc
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2021-05-16 17:17:23 +0100

    Update SearchWeather.jsx

--- src/component/SearchWeather.jsx
+++ src/component/SearchWeather.jsx
@@ -130,7 +130,7 @@ const SearchWeather = () => {
               >
                 <b>{desc}</b>
                 <img
-                  src={`https://openweathermap.org/img/wn/${icon}.png`}
+                  src={`http://openweathermap.org/img/wn/${icon}.png`}
                   alt=""
                   className="icon"
                 />
